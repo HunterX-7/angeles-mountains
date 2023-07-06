7e7ae9e59af5f412d6eeced1546622b5bf36d1b6
HunterX-7 <fraquin7@gmail.com>
2023-07-06 00:37:25 -0500

    Add the comps and import file

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import History from "./History";
+import Teams from "./Teams";
+import Footer from "./Footer";
+
+export {
+    Navbar, Hero, History, Teams, Footer,
+};
