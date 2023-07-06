41dd3a3e359d546d71f8a417da59072c72851b65
HunterX-7 <fraquin7@gmail.com>
2023-07-06 17:44:50 -0500

    Add styles to Navbar and Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start justify-center gap-5`}>
+        <div className='block items-center justify-center'>
+          <h1 className={`font-bebas font-semibold text-secondary md:text-[150px] sm:text-[90px] xs:text-[60px] text-[60px] md:leading-[120px] sm:leading-[80px] leading-[50px] mt-2`}>
+            LOS ANGELES 
+            <span className='block text-terciary tracking-wider'>MOUNTAINS</span>
+          </h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
 
 const Navbar = () => {
+  const [toogle, setToogle] = useState(false);
+  const [navbar, setNavbar] = useState(false);
+
+  const changeNavbar = () => {
+    if(window.scrollY >= 70) {
+      setNavbar(true)
+    } else {
+      setNavbar(false)
+    }
+  }
+
+  window.addEventListener('scroll', changeNavbar);
+
   return (
-    <div>Navbar</div>
+    <nav className={`z-10 w-full fixed flex py-2 justify-between items-center ${navbar ? 'navbar active ease-out duration-200' : 'navbar ease-out duration-200'}`}>
+      <div className='w-full flex sm:mx-32 mx-4 justify-between items-center'>
+        <a href="#" className='flex items-center cursor-pointer gap-2'>
+          <img src={logo} alt="Logo" className='w-[54px] h-[54px] object-contain' />
+          <p className={`text-secondary font-bebas leading-none tracking-normal text-[24px] font-semibold cursor-pointer mt-1 px-2
+            ${navbar ? '' : 'ease-out duration-200 hidden'}
+            `}
+          >
+            LOS ANGELES <span className='text-terciary block tracking-wider'>MOUNTAINS</span>
+          </p>
+        </a>
+        <ul className='list-none sm:flex hidden justify-end items-center flex-1 gap-20'>
+          {navLinks.map((nav) => (
+            <li key={nav.id} className={`font-lato italic font-black cursor-pointer text-[16px] ${navbar ? 'text-terciary' : 'text-white'} underline underline-offset-4 decoration-2`}>
+              <a href={`#${nav.id}`}>
+                {nav.number}
+                {nav.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+            <img 
+              src={toogle ? close : menu} 
+              alt="menu" 
+              className='w-[28px] h-[28px] object-contain' 
+              onClick={() => setToogle((prev) => !prev)} 
+            />
+            <div 
+              className={`${toogle ? 'flex' : 'hidden'} p-6 bg-gradient-to-r from-secondary to-terciary absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl`}
+            >
+              <ul className='list-none flex flex-col justify-end items-center flex-1 gap-5'>
+                {navLinks.map((nav) => (
+                  <li key={nav.id} className={`font-lato italic font-black cursor-pointer text-[16px] text-white underline underline-offset-4 decoration-2`}>
+                    <a href={`#${nav.id}`}>
+                      {nav.number}
+                      {nav.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
