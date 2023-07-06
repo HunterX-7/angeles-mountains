0134b62ee87def34baea1a5ba72d8b20c6caa84f
HunterX-7 <fraquin7@gmail.com>
2023-07-06 00:28:46 -0500

    Add assets and file imports

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import Logo from './Vector Smart Object.png';
+import Mountain from './mountain.jpg';
+import Peak from './peak.jpg';
+import Mountain1 from './mountain1.jpg';
+import Mountain2 from './mountain2.jpg';
+import Slider1 from './Slider1.jpg';
+import Slider2 from './Slider2.jpg';
+
+export {
+    Logo,
+    Mountain,
+    Peak,
+    Mountain1,
+    Mountain2,
+    Slider1,
+    Slider2,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,31 @@
+export const navLinks = [
+  {
+    id: "history",
+    number: "01.",
+    title: "History",
+  },
+  {
+    id: "teams",
+    number: "02.",
+    title: "Teams",
+  },
+];
+
+export const schedule = [
+    {
+      id: "vestibulum viverra1",
+      title: "Vestibulum viverra",
+    },
+    {
+      id: "vestibulum viverra2",
+      title: "Vestibulum viverra",
+    },
+    {
+      id: "vestibulum viverra3",
+      title: "Vestibulum viverra",
+    },
+    {
+      id: "vestibulum viverra4",
+      title: "Vestibulum viverra",
+    },  
+  ];
