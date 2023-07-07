02b55de6ecc501466406e64e40b9abb2ab61c0fc
HunterX-7 <fraquin7@gmail.com>
2023-07-07 00:17:25 -0500

    Update constants and components imports

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,9 @@ import Hero from "./Hero";
 import History from "./History";
 import Teams from "./Teams";
 import Footer from "./Footer";
+import Slider from "./Slider";
+import Accorddion from "./Accorddion";
 
 export {
-    Navbar, Hero, History, Teams, Footer,
+    Navbar, Hero, History, Teams, Footer, Slider, Accorddion,
 };
